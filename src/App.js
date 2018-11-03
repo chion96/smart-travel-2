a2c6dbdfd559b2fa875214842f17409147ff0e95
ChiOn <chion1996@gmail.com>
2018-11-03 21:23:57 +0800

    Added imges. Finished initial app bar and side bar.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,84 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import cathayLogo from './src/Cathay/element/bannerLogo.png';
+
 import './App.css';
 
+import { 
+  AppBar, 
+  Toolbar,
+  IconButton,
+  Menu,
+  MenuItem,
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import CenterFocusWeakIcon from '@material-ui/icons/CenterFocusWeak';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      sideBarOpen: false
+    };
+    this.toggleSideBar = this.toggleSideBar.bind(this);
+  }
+
+  toggleSideBar() {
+    this.setState({ sideBarOpen: !this.state.sideBarOpen });
+  }
+
   render() {
+    const { sideBarOpen } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
+
+          <AppBar position="static">
+            <Toolbar className="appToolBar">
+              <IconButton className="appBarButton measurementButton" color="inherit" aria-label="Measure">
+                <CenterFocusWeakIcon />
+              </IconButton>
+              <div className="headerTitle">
+                <img src={cathayLogo} />
+              </div>
+              <IconButton className="appBarButton notificationButton" color="inherit" aria-label="Notifications">
+                <NotificationsIcon />
+              </IconButton>
+              <IconButton className="appBarButton sideMenuButton" color="inherit" aria-label="Menu" onClick={this.toggleSideBar}>
+                <MenuIcon />
+              </IconButton>
+            </Toolbar>
+          </AppBar>
+          <Drawer
+            variant="persistent"
+            anchor="right"
+            open={sideBarOpen}
+          >
+            <div className="sideBarHeader">
+              <IconButton onClick={this.toggleSideBar}>
+                <ChevronLeftIcon />
+              </IconButton>
+            </div>
+            <Divider />
+            <List>
+              <ListItem button key="Home">
+                <ListItemIcon><InboxIcon /></ListItemIcon>
+                <ListItemText primary="Home" />
+              </ListItem>
+            </List>
+          </Drawer>
+
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
