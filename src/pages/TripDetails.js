32d01ebfde52236ee76a05bc608a02c003174416
ChiOn <chion1996@gmail.com>
2018-11-04 01:59:08 +0800

    Finished TripDetails (no luggage).

--- src/pages/TripDetails.js
+++ src/pages/TripDetails.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import beijingImg from '../src/beijing.png';
+import flightDetailBG from '../src/flightDetailsBanner2.png';
 
 import {
 	Card,
@@ -8,12 +9,21 @@ import {
 	CardContent,
 	Typography
 } from '@material-ui/core';
+import WarningIcon from '@material-ui/icons/Warning';
 
 import './css/TripDetails.css';
 
 class TripDetails extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { 
+			luggages: []
+		};
+	}
 
 	render() {
+		const { luggages } = this.state;
+
 		return (
 			<div className="trip-details-container">
 				<div>
@@ -45,19 +55,108 @@ class TripDetails extends Component {
 				<div className="content-div">
 					<Card className="flight-detail-card">
 						<CardContent className="flight-detail-card-content">
+							<div className="flight-detail-header">
+								<img className="flight-detail-header-image" src={flightDetailBG}/>
 
+								<div className="flight-detail-date flight-detail-word-in-pic">
+									02 Jan
+								</div>
+
+								<div className="flight-detail-from flight-detail-word-in-pic">
+									Hong Kong
+								</div>
+
+								<div className="flight-detail-to flight-detail-word-in-pic">
+									Beijing
+								</div>
+							</div>
+							<div className="flight-detail-detail">
+								<div className="flight-detail-field">
+									<div className="flight-detail-field-label">
+										Departure 
+									</div>
+
+									<div className="flight-detail-field-value">
+										Hong Kong
+									</div>
+								</div>
+
+								<div className="flight-detail-field">
+									<div className="flight-detail-field-label">
+										Destination
+									</div>
+
+									<div className="flight-detail-field-value">
+										Beijing
+									</div>
+								</div>
+
+								<div className="flight-detail-field">
+									<div className="flight-detail-field-label"> 
+										Departure Time
+									</div>
+
+									<div className="flight-detail-field-value">
+										02 Jan 19 15:45
+									</div>
+								</div>
+
+								<div className="flight-detail-field">
+									<div className="flight-detail-field-label"> 
+										Arrival Time
+									</div>
+
+									<div className="flight-detail-field-value">
+										02 Jan 19 18:22
+									</div>
+								</div>
+
+								<div className="flight-detail-field">
+									<div className="flight-detail-field-label"> 
+										Baggage Allowance(s)
+									</div>
+
+									<div className="flight-detail-field-value">
+										32 KG
+									</div>
+								</div>
+							</div>
 						</CardContent>
 					</Card>
 				</div>
 
 				<div className="content-div">
-					<div className="content-header">
-						Checked In Luggage(s)
-					</div>
+					{luggages.length <= 0 && (
+						<div>
+							<div className="no-luggage-header">
+								No luggage has been checked in yet
+							</div>
 
-					<div>
-					</div>
+							<div className="no-luggage-alert">
+								<div className="no-luggage-alert-icon">
+									<WarningIcon />
+								</div>
+
+								<div className="no-luggage-alert-text">
+									Press the above alert button to report any incident
+								</div>
+							</div>
+						</div>
+					)}
+
+					{luggages.length > 0 && (
+						<div>
+							<div className="content-header">
+								Checked In Luggage(s)
+							</div>
+
+							<div>
+								
+							</div>
+						</div>
+					)}
 				</div>
+				
 			</div>
 		)
 	}
